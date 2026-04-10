cb094882af6775c9ab5ba003d9559ae1dd9029ab
P257211 <P257211@mnservice.co.kr>
2026-04-10 17:51:13 +0900

    Create README.md

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from domain.question import question_router
 app = FastAPI()
 
 origins = [
-    "http://127.0.0.1:5173",
+    "http://localhost:3000",  # 로컬 접속허용
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=origins, # 허용할 오리진 목록
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"], # 모든 HTTP 메서드 허용 (GET, POST 등)
+    allow_headers=["*"], # 모든 헤더 허용
 )
 
 app.include_router(question_router.router)
